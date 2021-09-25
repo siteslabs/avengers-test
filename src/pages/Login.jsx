5b0ebb2f638fddcd06bf57e5009fd25d5eb60f3e
beksultan <beksultan.mamashov@gmail.com>
2021-09-26 01:39:57 +0600

    deleted useless files

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { Redirect } from "react-router-dom"
-import constants from "../redux/constants"
 import styles from "../styles/login.module.css"
 import { loginAC } from "../redux/actions/login"
 
