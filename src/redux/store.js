6040ec115331f9325cf74780c9285fc4a1291591
beksultan <beksultan.mamashov@gmail.com>
2021-09-26 01:36:29 +0600

    redux compose

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,4 @@
-import { applyMiddleware, combineReducers, createStore } from "redux"
+import { applyMiddleware, combineReducers, compose, createStore } from "redux"
 import thunk from "redux-thunk"
 import loginReducer from "./reducers/loginReducer"
 import transactionsReducer from "./reducers/transactionReducer"
